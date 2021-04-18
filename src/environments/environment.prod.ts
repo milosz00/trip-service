93d95d187007ed5c1f76cf8a0c78395322a4f507
milosz00 <milosz.czarnik00@gmail.com>
2021-04-18 13:35:09 +0200

    modified environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDi-FnBns9F7gwou7hlKObNSAm3J7ZULAU",
+    authDomain: "trip-list-169b2.firebaseapp.com",
+    projectId: "trip-list-169b2",
+    storageBucket: "trip-list-169b2.appspot.com",
+    messagingSenderId: "755775937822",
+    appId: "1:755775937822:web:fc298ab92bfcb0b3d57d0f",
+    measurementId: "G-Z0Z5KGJSLC"
+  },
+};
